ae52eee129c0ae98b40326d7bf7620c239f4bac5
ntcong1 <ntcong1@NTCONG1>
2020-11-24 17:15:20 +0700

    update cli

--- main.go
+++ main.go
@@ -1,32 +1,124 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"kafka-repush/services"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 )
-var(
-	brokers = []string{"192.168.75.132:9092"}
 
+type (
+	serviceConfig struct {
+		isDefault        bool
+		logFileName      string
+		lastLineFileName string
+		failPushFileName string
+		cronFormat       string
+		broker			[]string
+	}
+)
+
+var (
+//brokers = []string{"192.168.75.132:9092"}
+	config serviceConfig
 )
 
 func main() {
+
+	defaultCmd := flag.NewFlagSet("default", flag.ExitOnError)
+	defLogName := defaultCmd.String("log-name", "", "Log file name")
+	defLastLineName := defaultCmd.String("last-line-name", "", "File name to store last readied line")
+	defFailName := defaultCmd.String("fail-name", "", "File name to store failed push")
+	defBroker := defaultCmd.String("broker", "", "Kafka broker")
+
+	scheduleCmd := flag.NewFlagSet("schedule", flag.ExitOnError)
+	slogName := scheduleCmd.String("log-name", "", "Log file name")
+	sLastLineName := scheduleCmd.String("last-line-name", "", "File name to store last readied line")
+	sFailName := scheduleCmd.String("fail-name", "", "File name to store failed push")
+	sBroker := scheduleCmd.String("broker", "", "Kafka broker")
+	cronFormat := scheduleCmd.String("cron-format", "", "Cron format")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'default' or 'schedule' subcommands")
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "default":
+
+		defaultCmd.Parse(os.Args[2:])
+		fmt.Println("Service run with default config")
+		fmt.Println("  Log file's name:", *defLogName)
+		fmt.Println("  Last line file's name:", *defLastLineName)
+		fmt.Println("  Fail file's Name:", *defFailName)
+		fmt.Println("  Broker:", *defBroker)
+
+		config = serviceConfig{
+			isDefault:        true,
+			logFileName:      *defLogName,
+			lastLineFileName: *defLastLineName,
+			failPushFileName: *defFailName,
+			broker: strings.Split(*defBroker," "),
+		}
+
+	case "schedule":
+
+		scheduleCmd.Parse(os.Args[2:])
+		fmt.Println("Service run with schedule config")
+		fmt.Println("  Log file's name:", *slogName)
+		fmt.Println("  Last line file's name:", *sLastLineName)
+		fmt.Println("  Fail file's Name:", *sFailName)
+		fmt.Println("  Broker:", *sBroker)
+		fmt.Println("  Cron format:", *cronFormat)
+
+		config = serviceConfig{
+			isDefault:        false,
+			logFileName:      *slogName,
+			lastLineFileName: *sLastLineName,
+			failPushFileName: *sFailName,
+			broker: strings.Split(*sBroker," "),
+			cronFormat:       *cronFormat,
+		}
+
+	default:
+		fmt.Println("Expected 'default' or 'schedule' subcommands")
+		os.Exit(1)
+	}
+
+	startService(config)
+
+}
+func startService(config serviceConfig) {
+
 	fmt.Println("---------------Starting service---------------")
-	producer,err := services.NewProducer(brokers)
+
+	producer,err := services.NewProducer(config.broker)
 	if err != nil{
 		log.Fatal(err)
 	}
+
 	serviceConfig := services.ServiceConfig{
-		FileConfig:    services.NewFileConfig(),
+		FileConfig:   services.FileConfig{
+			LogName:      config.logFileName,
+			LastLineName:   config.lastLineFileName,
+			FailPushName: config.failPushFileName,
+		},
 		KafkaProducer: producer,
-		Topic: "Bogie",
 	}
+
 	service := services.NewLogHandler(serviceConfig)
-	// 5 4 * * *  -- At 04:05
-	service.Start("@every 0h0m15s")
+
+	// Run with default setting and only run one time
+	if config.isDefault {
+		service.ReadLog()
+		return
+	}
+
+	// Run with schedule setting and run with cron config
+	service.Start(config.cronFormat)
 	exit := make(chan os.Signal, 1)
 	signal.Notify(exit, syscall.SIGTERM, syscall.SIGINT, os.Interrupt, os.Kill)
 	for {
@@ -38,5 +130,3 @@ func main() {
 		}
 	}
 }
-
-
--- services/service.go
+++ services/service.go
@@ -14,19 +14,9 @@ import (
 type (
 	StringProducerMessage string
 
-	ReqResInfo struct {
-		TimeStamp     time.Time   `json:"timestamp"`
-		Level         string      `json:"level"`
-		CorrelationID string      `json:"correlationId"`
-		Version       string      `json:"version"`
-		RequestURL    string      `json:"requestURL"`
-		RequestMethod string      `json:"requestMethod"`
-		ServiceName   string      `json:"serviceName"`
-		FunctionName  string      `json:"functionName"`
-		StatusCode    string      `json:"statusCode"`
-		ResponseTime  string      `json:"responseTime"`
-		Request       interface{} `json:"request"`
-		Response      interface{} `json:"response"`
+	LogInfo struct {
+		Topic   string `json:"topic"`
+		Message string `json:"message"`
 	}
 
 	LogHandler interface {
@@ -40,7 +30,7 @@ type (
 
 	FileConfig struct {
 		LogName      string
-		OffsetName   string
+		LastLineName string
 		FailPushName string
 	}
 	offsetInfo struct {
@@ -49,7 +39,6 @@ type (
 	ServiceConfig struct {
 		FileConfig    FileConfig
 		KafkaProducer sarama.SyncProducer
-		Topic         string
 	}
 )
 
@@ -59,16 +48,8 @@ var (
 )
 
 // Key ..
-func (r ReqResInfo) Key() string {
-	return r.CorrelationID
-}
-
-func NewFileConfig() FileConfig {
-	return FileConfig{
-		LogName:      "log.txt",
-		OffsetName:   "lastLine.txt",
-		FailPushName: "fail",
-	}
+func (r LogInfo) Key() string {
+	return r.Topic
 }
 
 func NewLogHandler(config ServiceConfig) LogHandler {
@@ -81,12 +62,12 @@ func (handler *logHandler) ReadLog() {
 
 	lastLine, err := getLastLine()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
 
 	logFile, err := os.Open(serviceConfig.FileConfig.LogName)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
 
 	scanner := bufio.NewScanner(logFile)
@@ -96,33 +77,39 @@ func (handler *logHandler) ReadLog() {
 	for scanner.Scan() {
 		newLastLine++
 		if newLastLine > lastLine {
-			var reqResInfo ReqResInfo
-			err := json.Unmarshal(scanner.Bytes(), &reqResInfo)
+			var logInfo LogInfo
+			err := json.Unmarshal(scanner.Bytes(), &logInfo)
 			if err != nil {
-				fmt.Println(err)
+				log.Fatalln(err)
 			}
-			err = handler.Send(serviceConfig.Topic, reqResInfo)
+			err = handler.Send(logInfo.Topic, logInfo)
 			if err != nil {
-				fmt.Println("Fail sending message to kafka server" )
-				writeFailPush(scanner.Text())
+				fmt.Println("Fail sending message to kafka server")
+				err = writeFailPush(scanner.Text())
 			}
-			fmt.Printf(" Topic: %s || Message: %s \n", serviceConfig.Topic, reqResInfo)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Send success Topic: %s || Message: %s \n", logInfo.Topic, logInfo)
 		}
 	}
 
 	if scanner.Err() != nil {
-		fmt.Printf("Error reading logfile file: %s ", scanner.Err())
+		log.Fatalln(scanner.Err())
 	}
 
 	err = storeOffset(newLastLine)
 	if err != nil {
-		fmt.Printf("Error saving last line: %s ", err)
+		log.Fatalln(err)
 	}
 	return
 }
 
 func getLastLine() (int64, error) {
-	offsetFile, err := os.Open(serviceConfig.FileConfig.OffsetName)
+	if !isExist(serviceConfig.FileConfig.LastLineName){
+		return  0,nil
+	}
+	offsetFile, err := os.Open(serviceConfig.FileConfig.LastLineName)
 	if err != nil {
 		return 0, err
 	}
@@ -144,7 +131,7 @@ func getLastLine() (int64, error) {
 }
 
 func storeOffset(lineNum int64) error {
-	f, err := os.Create(serviceConfig.FileConfig.OffsetName)
+	f, err := os.Create(serviceConfig.FileConfig.LastLineName)
 	if err != nil {
 		return err
 	}
@@ -169,27 +156,33 @@ func storeOffset(lineNum int64) error {
 func writeFailPush(msg string) error {
 	f, err := os.OpenFile(serviceConfig.FileConfig.FailPushName+"_"+time.Now().Format("01-02-2006")+".txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	if _, err := f.Write([]byte(msg + "\n")); err != nil {
-		log.Fatal(err)
+		return err
 	}
 	if err := f.Close(); err != nil {
-		log.Fatal(err)
+		return err
 	}
 	return nil
 }
 
 func (handler *logHandler) Start(cronFormat string) {
 	cronService = cron.New()
-	cronService.AddFunc(cronFormat, handler.ReadLog)
+	_, err := cronService.AddFunc(cronFormat, handler.ReadLog)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	cronService.Start()
 }
 
 func (*logHandler) Close() {
 	//Closing cron and kafka
 	cronService.Stop()
-	serviceConfig.KafkaProducer.Close()
+	err := serviceConfig.KafkaProducer.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func (*logHandler) Send(topic string, msg ProducerMessage) error {
@@ -205,4 +198,11 @@ func (*logHandler) Send(topic string, msg ProducerMessage) error {
 	}
 	_, _, err = serviceConfig.KafkaProducer.SendMessage(kafkaMsg)
 	return err
+}
+
+func isExist(fileDir string) bool {
+	if _, err := os.Stat(fileDir); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
